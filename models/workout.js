0b52c297a01d5d8949206a4a54bc1ea71b192abf
CaroKris <ckhilpisch@gmail.com>
2021-04-12 19:42:16 -0500

    fixing model to include exercies as an array

--- models/workout.js
+++ models/workout.js
@@ -3,54 +3,60 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  type: {
-    type: String,
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercise: [{
+    type: {String,
     trim: true,
     required: "Enter the type of exercise"
   },
+
   name: {
     type: String,
     trim: true,
     required: "Enter a name for the exercise"
   },
+
   duration: {
     type: Number,
     required: "Enter the time spent"
   },
+
   distance: {
     type: Number,
     required: "Enter the distance covered"
   },
+
   weight: {
     type: Number,
     required: "Enter the weight used"
   },
+
   reps: {
     type: Number,
     required: "Enter the number of reps"
   },
+
   set: {
     type: Number,
     required: "Enter the number of sets"
   },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+}],
 });
 
 const workout = mongoose.model("workout", workoutSchema);
 
-db.workout.aggregate ( [
+db.workout.aggregate ([
   {
-    $addFields: {
-      totalWeight: { $muliply: ["$weight", "$rep", "$set"] }
-    }
+    $addFields: { 
+      totalWeight: { $sum: ["$weight"]}}
   },
   {
     $addFields: {
-      totalDistance:
-      { $sum: ["$distance"]}
+      totalDuration:
+      { $sum: ["$time"]}
     }
   }
   
